f8c483560b509e25def5af1785f43a2e41806138
Renan Mello Rangel de Oliveira <renan.oliveira@radixeng.com.br>
2018-11-03 02:16:29 -0300

    error handling

--- src/index.js
+++ src/index.js
@@ -8,12 +8,28 @@ const port = process.env.port || 3000;
 
 
 app.get('/', (req, res) => {
-   res.send('hello world!'); 
+  res.status(200).send('hello world!'); 
 });
 
 app.use('/pessoa', routePessoa);
 
 
-app.listen(port, function () {
-    console.log('Example app listening on port 3000!');
+app.use((req, res, next) => {
+  const err = new Error('Resource not found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    erro: {
+      mensagem: err.message
+    }
   });
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
