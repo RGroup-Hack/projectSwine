d731205cca4ff76800e9a544b11318c1b416dbb2
Renan Mello Rangel de Oliveira <portela.ramon96@gmail.com>
2018-11-03 21:26:37 -0300

    socket events

--- src/routes/socket.js
+++ src/routes/socket.js
@@ -1,3 +1,5 @@
+const getDistanceBetweenPeople = require('../utils/distance');
+
 module.exports = function(io){
     io.origins(['*:*', 'http://localhost:5500']);
 
@@ -8,16 +10,95 @@ module.exports = function(io){
         catchAssist: 'catchAssist',
         requestAssist: 'requestAssist',
         receiveAssist: 'receiveAssist',
-        receiveEndAssist: 'receiveEndOfAssist'
+        receiveEndAssist: 'receiveEndOfAssist',
+        findPeople: 'findPeople',
     }
+
+    const connected = new Array();
+    const needingHelp = new Array();
+    const pairs = new Array();
+
+    const posFix = 'Response';
 
     io.on('connection', (socket) => {
         console.log('connected');
 
-        socket.on(events.endOfTracking, (data) => {
-            console.log(data.over);
-            socket.emit('teste', {msg: 'hello from server!'});
-        })
-    });
+        connected.push(socket);
 
+        socket.on(events.endOfTracking, (data) => {
+            console.log(`event: ${events.endOfTracking}`);
+            socket.emit(`${events.endOfTracking}${posFix}`, {msg: `hello from server! - ${events.endOfTracking}`});
+        });
+        socket.on(events.sendMyPosition, (data) => {
+            console.log(`event: ${events.sendMyPosition}`);
+            socket.emit(`${events.sendMyPosition}${posFix}`, {msg: `hello from server! - ${events.sendMyPosition}`});
+        });
+        socket.on(events.receiveAnotherPosition, (data) => {
+            console.log(`event: ${events.receiveAnotherPosition}`);
+            socket.emit(`${events.receiveAnotherPosition}${posFix}`, {msg: `hello from server! - ${events.receiveAnotherPosition}`});
+        });
+        socket.on(events.findPeople, (data) => {
+            console.log('oi');
+            const { name, position } = data;
+    
+            const helper = {
+                socket,
+                name,
+                position
+            }
+
+            const needingHelpList = needingHelp.filter((helped) => {
+                const p1 = {lat1: helped.position.latitude, lon1: helped.position.longitude};
+                const p2 = {lat2: helper.position.latitude, lon2: helper.position.longitude};
+
+                const d = getDistanceBetweenPeople(p1, p2);  
+                console.log(d);
+
+                if(d <= maxDistance){
+                    helped.distance = d;
+                }
+
+                return d <= maxDistance;
+            })
+            .map((helped) => {
+                return {
+                    name: helped.name,
+                    location: helped.location,
+                    socketId: helped.socket.id,
+                }
+            });    
+            //console.log(`${helpers.map((person) => `${person.name} ${person.socket.id}`).join(' | ')}`);    
+            console.log(`event: ${events.findPeople}`);
+            socket.emit(`${events.findPeople}${posFix}`, {msg: `hello from server! - ${events.findPeople}`, list: needingHelpList});
+
+        });
+        socket.on(events.catchAssist, (data) => {
+
+        });
+        socket.on(events.requestAssist, (data) => {
+            const maxDistance = 0.1;
+
+            const { name, position, destination } = data;
+            const helped = {
+                socket,
+                name,
+                position,
+                destination,
+            }
+
+            needingHelp.push(helped);
+
+            console.log(`event: ${events.requestAssist}`);
+            console.log(`${needingHelp.map((person) => `${person.name} ${person.socket.id}`).join(' | ')}`);
+            socket.emit(`${events.requestAssist}${posFix}`, {msg: `hello from server! - ${events.requestAssist}`});
+        });
+        socket.on(events.receiveAssist, (data) => {
+            console.log(`event: ${events.receiveAssist}`);
+            socket.emit(`${events.receiveAssist}${posFix}`, {msg: `hello from server! - ${events.receiveAssist}`});
+        });
+        socket.on(events.receiveEndAssist, (data) => {
+            console.log(`event: ${events.receiveEndAssist}`);
+            socket.emit(`${events.receiveEndAssist}${posFix}`, {msg: `hello from server! - ${events.receiveEndAssist}`});
+        });
+    });
 };
---
+++ src/utils/distance.js
@@ -0,0 +1,19 @@
+const toRadians = function(degree){
+    const pi = Math.PI;
+    return degree * (pi / 180);
+}
+
+module.exports = function(p1, p2){
+    const {lat1, lon1} = p1;
+    const {lat2, lon2} = p2;
+
+    const R = 6371;
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d;    
+}
